219a7900a1923ab8c9f4e3334ec6b5091cc2d0dd
Administrator <Administrator@habiru-PC>
2017-09-12 16:11:05 +0900

    update method to express simple msg

--- src/newpract/cal/Newpact.java
+++ src/newpract/cal/Newpact.java
@@ -1,26 +1,30 @@
 package newpract.cal;
 
+import java.util.Calendar;
 import java.util.Scanner;
-
 public class Newpact {	
 	private static final int[] mon={31,28,31,30,31,30,31,31,30,31,30,31};
   
-	public int maxDaysCalculate(int i){
+	public int getMaxDaysOfMonth(int i){
    	return mon[i-1];
 	}
-	
-	
-	public static void main(String[] args) {
-		System.out.println("일   월   화     수   목    금    토");
+		
+	public void printSampleCalendar(){
+		System.out.println("일  월   화   수  목   금   토");
 		System.out.println("1  2  3  4  5  6  7");
 		System.out.println("8  9  10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 		
+	}
+	public static void main(String[] args) {		
+		
 //		입력받은 숫자 달의 최대 일수 출력하기
 		int i=0;
 		String str;
 		Scanner scan= new Scanner(System.in);
+		Calendar cal=Calendar.getInstance();
+		System.out.println("달을 입력하세요");
 //		str=scan.next();
 //		i=Integer.parseInt(str);
 		i=scan.nextInt();
@@ -73,10 +77,15 @@ public class Newpact {
 		System.out.println("31days");
 			break;		
 	}	
+	Newpact npt=new Newpact();
+	System.out.println(i+ "월은"+npt.getMaxDaysOfMonth(i)+"일 입니다");
+     npt.printSampleCalendar();
+	 scan.close();
 	
-	
-	 System.out.println(i+ "월은"  +(mon[i-1])+"일 까지이다");
 	}
-	
-}	
+
+
+	}
+	 
+
 
