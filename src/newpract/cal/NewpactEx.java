e4f83e415bbbc5dbc1c325995be91e4e77d84fe5
Administrator <Administrator@habiru-PC>
2017-09-13 11:34:36 +0900

    change sysout print method to int

--- src/newpract/cal/NewpactEx.java
+++ src/newpract/cal/NewpactEx.java
@@ -4,26 +4,28 @@ import java.util.Calendar;
 import java.util.Scanner;
 
 public class NewpactEx {
-//    private static int []month={31,28,31,30,31,30,31,31,30,31,30,31};
-	public static int getMaxDaysOfMonth(int month){	
-		if(month==2){
-			return 28;			
-		}else if(month==4||month==6||month==9||month==11){
-			return 30;
-		}else{
-			return 31;
-		}
-		 
-	
+  private static int []month={31,28,31,30,31,30,31,31,30,31,30,31};
+	public static int getMaxDaysOfMonth(int i) {
+	       return month[i-1];	
 	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String PROMPT="<cal> ";
 		System.out.println("월을 입력하세요");
-		Scanner scan= new Scanner(System.in);
-//		Calendar cal=Calendar.getInstance();
-		int month=scan.nextInt();	
-	System.out.println(month+ "월은"+ getMaxDaysOfMonth(month) +"일입니다");
-	scan.close();
-	}
+		Scanner scan = new Scanner(System.in);
+//		 Calendar cal=Calendar.getInstance();
+		int repeat = scan.nextInt();		
+		
+		for(int i=0; i<repeat; i++){
+//			System.out.println("달을 입력하세요");
+			System.out.println(PROMPT);
+			int month=scan.nextInt();
+			System.out.println(month + "월은" + (int)getMaxDaysOfMonth(month) + "일입니다");
+		}
+		
 	
+		scan.close();
+	}
+
 }
